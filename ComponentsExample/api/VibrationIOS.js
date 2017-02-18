366a553d59c455ed40a92f660b8fb3c1072e1c33
Ben <dpc323@126.com>
2017-02-18 11:43:29 +0800

    遗漏两个文件

---
+++ ComponentsExample/api/VibrationIOS.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  VibrationIOS
+} = ReactNative;
+
+exports.framework = 'React';
+exports.title = 'VibrationIOS';
+exports.description = 'Vibration API for iOS';
+exports.examples = [{
+  title: 'VibrationIOS.vibrate()',
+  render() {
+    return (
+      <TouchableHighlight
+        style={styles.wrapper}
+        onPress={() => VibrationIOS.vibrate()}>
+        <View style={styles.button}>
+          <Text>Vibrate</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  },
+}];
+
+var styles = StyleSheet.create({
+  wrapper: {
+    borderRadius: 5,
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: '#eeeeee',
+    padding: 10,
+  },
+});
